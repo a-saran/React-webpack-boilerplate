e6af6063e792d2de09167c69dd84142c579b0617
asaran97 <asaran234@gmail.com>
2020-05-31 00:17:01 +0530

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <h1>initial app</h1>
+      <Router>
+        <div className="App">
+          <Link to='/'>home</Link>
+          <Link to='/about'>about</Link>
+
+          <Switch>
+            <Route exact path="/" render={() => <h1>home page</h1>} />
+            <Route exact path="/about" render={() => <h1>about page</h1>} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   )
 }
 
-export default App
+export default App;
